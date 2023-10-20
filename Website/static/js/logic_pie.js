c59b455cfaa361c4886a70eae754a5ee9757806d
Faith-Hall <135525815+Faith-Hall@users.noreply.github.com>
2023-10-19 22:33:57 -0500

    drop down menu

--- Website/static/js/logic_pie.js
+++ Website/static/js/logic_pie.js
@@ -3,7 +3,7 @@ const url = "http://127.0.0.1:5000/raw-lung-cancer-data";
 
 // Promise pending
 const dataPromise = d3.json(url);
-console.log(states);
+console.log("Data Promise:",dataPromise);
 
 d3.json(url).then(function(data) {
     console.log(data);
@@ -17,14 +17,30 @@ function init() {
     // Use D3 to get sample names and populate the drop-down selector
     d3.json(url).then((data) => {
         
-        // Set a variable for the state names
-        let states = [];
+    // Empty list for states 
+    let states = []
 
-        // Add samples to dropdown menu
+    // Loop to filter out the states
+    data.forEach((state) => {
+        console.log(states)
+        state = state.LocationAbbr
+        console.log(state)
+        states.push(state)
+    });
+    console.log(states)
+
+    states = states.filter((item, 
+        index) => states.indexOf(item) === index);
+        // Loop through states and append
         states.forEach((name) => {
             console.log(states);
             dropdownMenu.append("option").text(name).property("value",name);
+
+            // create pie chart
+            piechart = []
         });
     });
 };
 
+// Initialize
+init();
